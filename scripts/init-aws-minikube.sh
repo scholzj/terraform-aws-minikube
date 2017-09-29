1781f4a75347eb9cd84b7dd2a364dea772d515a1
Jakub Scholz <www@scholzj.com>
2017-09-29 23:34:20 +0200

    Start updating to Kubernetes 1.8.0

--- scripts/init-aws-minikube.sh
+++ scripts/init-aws-minikube.sh
@@ -9,7 +9,7 @@ export DNS_NAME=${dns_name}
 export IP_ADDRESS=${ip_address}
 export CLUSTER_NAME=${cluster_name}
 export ADDONS="${addons}"
-export KUBERNETES_VERSION="1.7.5"
+export KUBERNETES_VERSION="1.8.0"
 
 # Set this only after setting the defaults
 set -o nounset
