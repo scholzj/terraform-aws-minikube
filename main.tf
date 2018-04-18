ca1f850673421339897b78b765919fd78c18d95a
Jakub Scholz <www@scholzj.com>
2018-04-18 22:55:29 +0200

    Update to Kubernetes 1.10.1

--- main.tf
+++ main.tf
@@ -25,29 +25,29 @@ data "aws_subnet" "minikube_subnet" {
 
 resource "aws_security_group" "minikube" {
   vpc_id = "${data.aws_subnet.minikube_subnet.vpc_id}"
-  name = "${var.cluster_name}"
+  name   = "${var.cluster_name}"
 
   tags = "${merge(map("Name", var.cluster_name, format("kubernetes.io/cluster/%v", var.cluster_name), "owned"), var.tags)}"
 
   ingress {
-    from_port = 22
-    to_port = 22
-    protocol = "tcp"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-  
+
   ingress {
-    from_port = 6443
-    to_port = 6443
-    protocol = "tcp"
+    from_port   = 6443
+    to_port     = 6443
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
-      from_port = 0
-      to_port = 0
-      protocol = "-1"
-      cidr_blocks = ["0.0.0.0/0"]
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
@@ -95,7 +95,7 @@ resource "aws_iam_policy_attachment" "minikube-attach" {
 }
 
 resource "aws_iam_instance_profile" "minikube_profile" {
-  name  = "${var.cluster_name}"
+  name = "${var.cluster_name}"
   role = "${aws_iam_role.minikube_role.name}"
 }
 
@@ -108,19 +108,19 @@ data "template_file" "init_minikube" {
 
   vars {
     kubeadm_token = "${module.kubeadm-token.token}"
-    dns_name = "${var.cluster_name}.${var.hosted_zone}"
-    ip_address = "${aws_eip.minikube.public_ip}"
-    cluster_name = "${var.cluster_name}"
-    addons = "${join(" ", var.addons)}"
+    dns_name      = "${var.cluster_name}.${var.hosted_zone}"
+    ip_address    = "${aws_eip.minikube.public_ip}"
+    cluster_name  = "${var.cluster_name}"
+    addons        = "${join(" ", var.addons)}"
   }
 }
 
 data "template_file" "cloud-init-config" {
-    template = "${file("${path.module}/scripts/cloud-init-config.yaml")}"
+  template = "${file("${path.module}/scripts/cloud-init-config.yaml")}"
 
-    vars {
-        calico_yaml = "${base64gzip("${file("${path.module}/scripts/calico.yaml")}")}"
-    }
+  vars {
+    calico_yaml = "${base64gzip("${file("${path.module}/scripts/calico.yaml")}")}"
+  }
 }
 
 data "template_cloudinit_config" "minikube_cloud_init" {
@@ -145,7 +145,7 @@ data "template_cloudinit_config" "minikube_cloud_init" {
 ##########
 
 resource "aws_key_pair" "minikube_keypair" {
-  key_name = "${var.cluster_name}"
+  key_name   = "${var.cluster_name}"
   public_key = "${file(var.ssh_public_key)}"
 }
 
@@ -153,8 +153,9 @@ resource "aws_key_pair" "minikube_keypair" {
 # EC2 instance
 #####
 
-data "aws_ami_ids" "centos7" {
-  owners = ["aws-marketplace"]
+data "aws_ami" "centos7" {
+  most_recent = true
+  owners      = ["aws-marketplace"]
 
   filter {
     name   = "product-code"
@@ -173,44 +174,44 @@ data "aws_ami_ids" "centos7" {
 }
 
 resource "aws_eip" "minikube" {
-  vpc      = true
+  vpc = true
 }
 
 resource "aws_instance" "minikube" {
-    # Instance type - any of the c4 should do for now
-    instance_type = "${var.aws_instance_type}"
+  # Instance type - any of the c4 should do for now
+  instance_type = "${var.aws_instance_type}"
 
-    ami = "${data.aws_ami_ids.centos7.ids[0]}"
+  ami = "${data.aws_ami.centos7.id}"
 
-    key_name = "${aws_key_pair.minikube_keypair.key_name}"
+  key_name = "${aws_key_pair.minikube_keypair.key_name}"
 
-    subnet_id = "${var.aws_subnet_id}"
+  subnet_id = "${var.aws_subnet_id}"
 
-    associate_public_ip_address = false
+  associate_public_ip_address = false
 
-    vpc_security_group_ids = [
-        "${aws_security_group.minikube.id}"
+  vpc_security_group_ids = [
+    "${aws_security_group.minikube.id}",
+  ]
+
+  iam_instance_profile = "${aws_iam_instance_profile.minikube_profile.name}"
+
+  user_data = "${data.template_cloudinit_config.minikube_cloud_init.rendered}"
+
+  tags = "${merge(map("Name", var.cluster_name, format("kubernetes.io/cluster/%v", var.cluster_name), "owned"), var.tags)}"
+
+  root_block_device {
+    volume_type           = "gp2"
+    volume_size           = "50"
+    delete_on_termination = true
+  }
+
+  lifecycle {
+    ignore_changes = [
+      "ami",
+      "user_data",
+      "associate_public_ip_address",
     ]
-
-    iam_instance_profile = "${aws_iam_instance_profile.minikube_profile.name}"
-
-    user_data = "${data.template_cloudinit_config.minikube_cloud_init.rendered}"
-
-    tags = "${merge(map("Name", var.cluster_name, format("kubernetes.io/cluster/%v", var.cluster_name), "owned"), var.tags)}"
-
-    root_block_device {
-        volume_type = "gp2"
-	      volume_size = "50"
-	      delete_on_termination = true
-    }
-
-    lifecycle {
-      ignore_changes = [
-        "ami",
-        "user_data",
-        "associate_public_ip_address"
-      ]
-    }
+  }
 }
 
 resource "aws_eip_association" "minikube_assoc" {
