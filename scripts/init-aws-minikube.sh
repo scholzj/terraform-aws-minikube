a8bcae82757a7cd3e8db348304a2342565740dee
Jakub Scholz <www@scholzj.com>
2021-01-17 18:39:42 +0100

    Update to Kubernetes 1.20.2

--- scripts/init-aws-minikube.sh
+++ scripts/init-aws-minikube.sh
@@ -11,7 +11,7 @@ export DNS_NAME=${dns_name}
 export IP_ADDRESS=${ip_address}
 export CLUSTER_NAME=${cluster_name}
 export ADDONS="${addons}"
-export KUBERNETES_VERSION="1.20.1"
+export KUBERNETES_VERSION="1.20.2"
 
 # Set this only after setting the defaults
 set -o nounset
