091e9d9306fcb71a9cfc9c070031685b7572e7b8
Jakub Scholz <www@scholzj.com>
2026-03-29 20:36:07 +0200

    Update to Kube 1.35.3 and CRI-O 1.35.1
    Signed-off-by: Jakub Scholz <www@scholzj.com>

--- scripts/init-aws-minikube.sh
+++ scripts/init-aws-minikube.sh
@@ -14,7 +14,7 @@ export AWS_REGION=${aws_region}
 export ADDONS="${addons}"
 export KUBERNETES_VERSION="${kubernetes_version}"
 export KUBERNETES_REPO_VERSION="v1.35"
-export CRIO_VERSION="1.35.0"
+export CRIO_VERSION="1.35.1"
 export CRIO_REPO_VERSION="v1.35"
 
 # Set this only after setting the defaults
